6139819298cc0f87cac6904be0e845e3c2620c45
PrinceDEV-web <deekorprince360@gmail.com>
2023-06-12 07:56:13 +0100

    pushed the source code for this project to github

---
+++ account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+     # LANDING PAGE URL
+    path('', views.index, name="LandingPage"),
+    
+    path('login', views.account_login, name="account_login"),
+    path('register/', views.account_register, name="account_register"),
+    path('logout/', views.account_logout, name="account_logout"),
+]
