aba851df255ec871b39b0f3ec2f2359e3e76278d
Prince Deekor <deekorprince360@gmail.com>
2023-06-12 08:44:56 +0100

    Update settings.py

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     #   You can use this :
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),,
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),,
     }
 
     # 'default': {
