c54777e910f8f2795a4bdd6360b6b4815c02266c
PrinceDEV-web <deekorprince360@gmail.com>
2023-06-12 11:40:22 +0100

    my project commit

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 # Following settings only make sense in production and may break development environments.
 # Tell Django to copy statics to the `static files` directory
 # in your application directory on Render.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Turn on the WhiteNoise storage backend that takes care of compressing static files
 # and creating unique names for each version so they can safely be cached forever.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
